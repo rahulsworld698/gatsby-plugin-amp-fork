a4f1f8ffa3a688186616296f722bc847ec926a1c
nonjene <reanfn89@gmail.com>
2021-06-16 01:18:31 +0800

    ✅ Add test

---
+++ tests/onPreRenderHTML.test.js
@@ -0,0 +1,78 @@
+import { onPreRenderHTML } from '../src/gatsby-ssr';
+
+const replaceHeadComponents = jest.fn();
+const replacePreBodyComponents = jest.fn();
+const replacePostBodyComponents = jest.fn();
+
+const mockParams =
+    ({ excludedPaths } = {}) =>
+    ({ pathname }) => {
+        const gatsbyHelper = {
+            getHeadComponents: () => [],
+            replaceHeadComponents,
+            getPreBodyComponents: () => [],
+            replacePreBodyComponents,
+            getPostBodyComponents: () => [],
+            replacePostBodyComponents,
+            pathname,
+        };
+        const option = {
+            analytics: true,
+            canonicalBaseUrl: 'https://foo.com/',
+            components: [],
+            includedPaths: [],
+            excludedPaths,
+        };
+
+        return [gatsbyHelper, option];
+    };
+
+describe('onPreRenderHTML - replaceHeadComponents', () => {
+    beforeEach(() => {
+        replaceHeadComponents.mockReset();
+        replacePreBodyComponents.mockReset();
+        replacePostBodyComponents.mockReset();
+    });
+
+    it('when is amp, should append some amp script tag', () => {
+        onPreRenderHTML(
+            ...mockParams()({
+                pathname: '/amp/',
+            })
+        );
+        expect(replaceHeadComponents.mock.calls).toMatchSnapshot();
+    });
+
+    describe('when is not amp & set excludePath to ["/foo/**"]', () => {
+        const mockOption = mockParams({
+            excludedPaths: ['/foo/**'],
+        });
+
+        it('when pathname is `/foo/bar`, should not call replaceHeadComponents', () => {
+            onPreRenderHTML(
+                ...mockOption({
+                    pathname: '/foo/bar',
+                })
+            );
+            expect(replaceHeadComponents).not.toBeCalled();
+        });
+
+        it('when pathname is `/foo/bar`, should not call replaceHeadComponents', () => {
+            onPreRenderHTML(
+                ...mockOption({
+                    pathname: '/foo/bar',
+                })
+            );
+            expect(replaceHeadComponents).not.toBeCalled();
+        });
+
+        it('when pathname is `/fooo/bar`, should call replaceHeadComponents', () => {
+            onPreRenderHTML(
+                ...mockOption({
+                    pathname: '/fooo/bar',
+                })
+            );
+            expect(replaceHeadComponents.mock.calls).toMatchSnapshot();
+        });
+    });
+});
